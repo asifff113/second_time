bd647651a2a7bcc5085f912f490aa5cb55eb8685
unknown <asifff113>
2023-04-18 12:37:58 +0600

    1.0

---
+++ array to reverse function.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main()
+{ int ara[10]={10,23,45,6,7,7564,53,234,23,10};
+
+    printf("the first element :%d\n ",ara[0]);
+    printf("the third  element :%d2\n ",ara[2]);
+    int i ;
+    for(i=9 ; i>=0 ; i--){
+        printf(" %d th element is : %d\n",i+1,ara[i]);
+
+    }
+
+
+return 0;
+}
+
+
